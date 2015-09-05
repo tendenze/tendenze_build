b49a092e2b37e516cf19d7b68df5fd8db2c8385f
tendenze <tendenze>
2015-09-05 17:25:29 +0200

    Update.

--- js/lineup.js
+++ js/lineup.js
@@ -1,9 +1,39 @@
-function init() {
-  var img_defer = $(".band .image img");
-  for(var i = 0; i < img_defer.length; i++) {
-    if(img_defer[i].getAttribute("data-src")) {
-      img_defer[i].setAttribute("src", img_defer[i].getAttribute("data-src"));
+function trigger_image_loading(placeholder) {
+  var num = placeholder.getAttribute("data-trigger");
+  var images = $(".band .image img[data-trigger='" + num + "']");
+  for(var i = 0; i < images.length; i++) {
+    if(images[i].getAttribute("data-src")) {
+      images[i].setAttribute("src", images[i].getAttribute("data-src"));
     }
   };
 }
-window.onload = init;
+
+function check_placeholders(placeholders) {
+  var idx = -1;
+  for (var i = 0; i < placeholders.length; i++) {
+    if($(placeholders[i]).offset().top - ($(window).scrollTop() + $(window).height()) < 200) {
+      trigger_image_loading(placeholders[i]);
+      idx = i;
+    }
+  };
+  if(idx !== -1) {
+    placeholders.splice(0, idx + 1);
+  }
+}
+
+var scroll_timeout = null;
+var placeholders = $(".band .image .placeholder");
+window.onload = check_placeholders(placeholders);
+
+function scroll_handler() {
+  check_placeholders(placeholders);
+}
+
+$(window).scroll(function() {
+  if(placeholders.length === 0) return;
+  if(scroll_timeout) {
+    window.clearTimeout(scroll_timeout);
+    scroll_timeout = null;
+  }
+  scroll_timeout = window.setTimeout(scroll_handler, 300);
+});
